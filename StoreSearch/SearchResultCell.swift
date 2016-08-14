18eede734cd0b4c3111e2f1042e630792365a23b
pafffcio <pawel.kubalewski@gmail.com>
2016-08-14 21:08:51 +0200

    upgrad

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,56 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by M.I. Hollemans on 19/08/15.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistNameLabel: UILabel!
+  @IBOutlet weak var artworkImageView: UIImageView!
+
+  var downloadTask: NSURLSessionDownloadTask?
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    let selectedView = UIView(frame: CGRect.zero)
+    selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+    selectedBackgroundView = selectedView
+  }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+  func configureForSearchResult(searchResult: SearchResult) {
+    nameLabel.text = searchResult.name
+    
+    if searchResult.artistName.isEmpty {
+      artistNameLabel.text = "Unknown"
+    } else {
+      artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName, searchResult.kindForDisplay())
+    }
+    
+    artworkImageView.image = UIImage(named: "Placeholder")
+    if let url = NSURL(string: searchResult.artworkURL60) {
+      downloadTask = artworkImageView.loadImageWithURL(url)
+    }
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    
+    downloadTask?.cancel()
+    downloadTask = nil
+    
+    nameLabel.text = nil
+    artistNameLabel.text = nil
+    artworkImageView.image = nil
+  }
+}
